81ea658e9c6c3755a8cf334c34c8f835cf66fc5b
jsully.k1 <jsully.k1@d2098b6b-32f0-6f3a-464b-ddd03dbbe399>
2011-02-21 19:39:16 +0000

    added structs files

---
+++ trunk/source/planner/avc2011Structs.h
@@ -0,0 +1,9 @@
+/////////////////////////////////////////////////////////////////////////////
+// File: avc2011Structs.h
+// This file contains structures used in multiple modules, such as
+// component vectors, state vectors, and the like.
+//
+typedef struct avcForceVector {
+  int x;
+  int y;
+} avcForceVector;
