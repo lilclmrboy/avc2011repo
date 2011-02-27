b3dfaa66dd817f574b082cfaec5a501a74863cd7
jsully.k1 <jsully.k1@d2098b6b-32f0-6f3a-464b-ddd03dbbe399>
2011-02-27 00:32:22 +0000

    added location sensors tea process

--- trunk/source/planner/locModule.cpp
+++ trunk/source/planner/locModule.cpp
@@ -14,7 +14,8 @@ avcStateVector
 avcPosition::getPosition(const avcForceVector& Control) {
 	
 	int curSec = getGPSTimeSec();
-	printf("Current GPS seconds in the day: %d", curSec);
+	printf("Current GPS seconds in the day: %d\n", curSec);
+	fflush(stdout);
 	return m_curPos;
 }
 
@@ -23,15 +24,14 @@ avcPosition::getPosition(const avcForceVector& Control) {
 int 
 avcPosition::getGPSTimeSec() {
 	int secs = 0;
-	unsigned char pData[6] = {0};
-	unsigned char index = 0x00;
-	m_pStem->VAL_SET(aGP2_MODULE, IIC_SPD_VAL_INDEX, IIC_400Khz);
-	m_pStem->IIC_XMIT(GPS_MODULE, &index, 1);
-	m_pStem->IIC_RD( aGP2_MODULE, GPS_MODULE, 0x06, pData);
-	secs = (pData[0] *10 + pData[1])* 3600 
-	       + (pData[2] * 10 + pData[3]) * 60 + (pData[4] * 10 + pData[5]);
-	
-	m_pStem->	
+	int tmp = 0;
+	tmp = m_pStem->PAD_IO(aGP2_MODULE, aSPAD_GP2_GPS_MIN) << 8; 
+  tmp |= m_pStem->PAD_IO(aGP2_MODULE, aSPAD_GP2_GPS_MIN+1);
+	secs = tmp * 60;
+	tmp = m_pStem->PAD_IO(aGP2_MODULE, aSPAD_GP2_GPS_SEC) << 8; 
+  tmp |= m_pStem->PAD_IO(aGP2_MODULE, aSPAD_GP2_GPS_SEC+1);
+	secs += tmp;
+		
 	return secs;	
 }
 
