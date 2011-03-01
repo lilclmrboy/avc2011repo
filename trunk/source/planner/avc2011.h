d769b75492e033aae4e23269e28b99c0b292fc74
mattkrugman@gmail.com <mattkrugman@gmail.com@d2098b6b-32f0-6f3a-464b-ddd03dbbe399>
2011-03-01 17:44:26 +0000

    Added missing global header file

---
+++ trunk/source/planner/avc2011.h
@@ -0,0 +1,32 @@
+/////////////////////////////////////////////////////////////////////////////
+// File: avc2011s.h
+// This file contain what we need to be consistent across project files
+//
+#ifndef _avc2011_H_
+#define _avc2011_H_
+
+// Acroname specific header files
+#include "aStem.h"
+
+// Standard libraries
+#include <vector>
+#include <stdarg.h>
+#include <stdio.h>
+#include <math.h>
+
+// Project specific headers
+#include "avc2011Structs.h"
+#include "avc2011Defs.tea"
+
+// Utilites
+#ifdef aDEBUG_H
+#define aDEBUG_PRINT(arg, ... ) printf(arg, ##__VA_ARGS__);fflush(stdout)
+#else 
+#define aDEBUG_PRINT(arg)
+#endif
+
+// Key values for settings file references
+// This takes an integer value (recommended 0-200 range)
+#define aKEY_VELOCITY_SETPOINT_MAX "setpoint_max"
+
+#endif //_avc2011_H_
