85d0df0d292aba39edf9b291c086a2df6ff71ecf
justin.l.gregg@gmail.com <justin.l.gregg@gmail.com@d2098b6b-32f0-6f3a-464b-ddd03dbbe399>
2012-06-15 03:57:28 +0000

    - enable EKF

--- trunk/source/planner/locModule.cpp
+++ trunk/source/planner/locModule.cpp
@@ -46,13 +46,10 @@ avcPosition::init(acpStem* pStem,
 
   if (m_pStem && m_pStem->isConnected()) {
 		
-    //Set current time.
-    aIO_GetMSTicks(m_ioRef, (unsigned long int*)&m_gpsClock, NULL);
-		
     //Lets initialize the encoders to zero.
     m_pStem->MO_ENC32(aMOTO_MODULE, ENCODER_IDX, 0);
     m_Encoder = 0;
-    m_lastDistanceTraveled = 0;
+    m_lastDistanceTraveled = 0.0;
 		
 #ifdef aUSE_GPS
 
@@ -117,14 +114,14 @@ avcPosition::init(acpStem* pStem,
     //Initialize the Variance matrix Q.
     //1 meter x and y, and 1 degree heading.
     // ~ .31 meters per reading. * 10% is .0312
-    m_Q(1,1) = aLAT_PER_METER * .0312;
-    m_Q(2,2) = aLON_PER_METER * .0312;
+    m_Q(1,1) = aLON_PER_METER * .0312;
+    m_Q(2,2) = aLAT_PER_METER * .0312;
     // heading variance
     m_Q(3,3) = DEG_TO_RAD * .25;
 
     //The GPS is accurate to a meter, but I don't really trust that.
-    m_W(1,1) = aLAT_PER_METER * 1.25;
-    m_W(2,2) = aLON_PER_METER * 1.25;
+    m_W(1,1) = aLON_PER_METER * 1.25;
+    m_W(2,2) = aLAT_PER_METER * 1.25;
     //Compass is accurate to 4 degrees.
     m_W(3,3) = DEG_TO_RAD * 2.5;
 
@@ -149,7 +146,8 @@ avcPosition::updateState() {
 	// we have stem processes running
 	long unsigned int curClock;
 	aIO_GetMSTicks(m_ioRef, &curClock, NULL);
-	long unsigned int tmElapsed = (curClock - m_curClock);
+	long unsigned int ticksElapsed = (curClock - m_curClock);
+  double tmElapsed = ticksElapsed/1000.0;
 	
 	// First we must do an estimation step, given the previous position
   // and the current control information. Lets do this in meters, and then
@@ -175,26 +173,35 @@ avcPosition::updateState() {
     m_logger->log(ERROR, "%s: Elapsed time is invalid", __FUNCTION__);
   
 	
-  double fVelocity =  m_metersPerTick * (double)(curEnc - m_Encoder) / ((double)tmElapsed / 1000.0);
+  double fVelocity =  m_metersPerTick * (double)(curEnc - m_Encoder) / tmElapsed;
   double fDistRolled =  m_metersPerTick * (double)(curEnc - m_Encoder);
-  m_logger->log(INFO, "Current Speed (m/s): %lf (tick/time=%d/%d)", fVelocity, (curEnc - m_Encoder), tmElapsed);
+  m_logger->log(INFO, "Current Speed (m/s): %lf (tick/time=%d/%lf)", fVelocity, (curEnc - m_Encoder), tmElapsed);
 	
   // These calculations are predictions of where we think we need to be
   // estimate change in x and y and heading
   // TODO - we should use previous state vector to calculate position here.
   // previous velocity.
-  double dx = sin(m_curPos.h * DEG_TO_RAD)* fDistRolled * aLON_PER_METER;
-  double dy = cos(m_curPos.h * DEG_TO_RAD)* fDistRolled * aLAT_PER_METER;
+  double predicted_dx = sin(m_curPos.h * DEG_TO_RAD)* fDistRolled * aLON_PER_METER;
+  double predicted_dy = cos(m_curPos.h * DEG_TO_RAD)* fDistRolled * aLAT_PER_METER;
     
   // Change in heading due to the previous steering angle
-  //double fRot = fDistRolled/m_wheelBase * tan(steerAngleRad) * RAD_TO_DEG;
+  double steerAngleRad = getSteeringAngleRad();
+  double predicted_dh = fDistRolled/m_wheelBase * tan(steerAngleRad);
   
-
+  // grab the current heading to enable observation of the change in heading
+  float currentHeading = 0.0;
+  if(0 != m_pCompass->getHeadingDeg(&currentHeading)){
+    m_logger->log(ERROR, "%s: Couldn't get new compass heading", __FUNCTION__);
+  }
+  
+  // maybe use the change in heading from the compass to "predict" the change in heading
+  //double predicted_dh = currentHeading - m_curPos.h;
 
   if(1){//(curEnc - m_Encoder) {
   	m_logger->log(INFO, "%s: --- fDist: %f", __FUNCTION__, fDistRolled);
-    m_logger->log(INFO, "%s: --- dx(m): %f", __FUNCTION__, dx/aLON_PER_METER);
-    m_logger->log(INFO, "%s: --- dy(m): %f", __FUNCTION__, dy/aLAT_PER_METER);
+    m_logger->log(INFO, "%s: --- dx(m): %f", __FUNCTION__, predicted_dx/aLON_PER_METER);
+    m_logger->log(INFO, "%s: --- dy(m): %f", __FUNCTION__, predicted_dy/aLAT_PER_METER);
+    m_logger->log(INFO, "%s: --- dh(m): %f", __FUNCTION__, predicted_dh);
     m_logger->log(INFO, "%s: --- hed: %f", __FUNCTION__, m_curPos.h);
   }
 	// Store current readings (for the next predict phase)
@@ -203,18 +210,18 @@ avcPosition::updateState() {
 	
   // Update the current control state vector
   // These vector values should be in latitude, longitude and degrees
-  m_curPos.x += dx;
-  m_curPos.y += dy;
-  m_pCompass->getHeadingDeg((float *)&m_curPos.h);
-  m_lastDistanceTraveled = fDistRolled;
-	
-	/*
+//  m_curPos.x += dx;
+//  m_curPos.y += dy;
+//  m_curPos.h = currentHeading;
+//  m_lastDistanceTraveled = fDistRolled;
+  
 	Matrix state(3,1);
 	//JLG flipped cos/sin - world cordinate system aligns latitude values with,
 	//values along the x axis, and longitude values along the y axis.
-	state(1,1) = dx + m_curPos.x;
-	state(2,1) = dy + m_curPos.y;
-	state(3,1) = m_curPos.h + (fRot * RAD_TO_DEG);
+	state(1,1) = m_curPos.x + predicted_dx;
+	state(2,1) = m_curPos.y + predicted_dy;
+	state(3,1) = m_curPos.h + predicted_dh;
+
   m_logger->log(INFO, "Set State: ");
 	//EKF from here on out.
 	//We need to calculate the probability matrix for the motion each time.
@@ -223,57 +230,51 @@ avcPosition::updateState() {
 	//Setting diags for F to 1, and G 33 to 1.
 	F(1,1) = F(2,2) = F(3,3) = 1;
 	//Calculating the Linearization Matrix points for F.
-	F(1,3) = -dx*sin(m_curPos.h) - dy*cos(m_curPos.h);
-	F(2,3) = dx*cos(m_curPos.h) - dy*cos(m_curPos.h);
+	F(1,3) = -1.0*predicted_dx;
+	F(2,3) = predicted_dy;
 	
 	//Calculate the Linearization Matrix points for G.
-	G(1,1) = cos(m_curPos.h); G(1,2) = -sin(m_curPos.h);
-  G(2,1) = sin(m_curPos.h); G(2,2) = cos(m_curPos.h);
-  G(3,3) = 1;
+  // m_curPos.h is still the previous heading
+	G(1,1) = tmElapsed*cos(m_curPos.h); 				G(1,2) = 0.0;
+  G(2,1) = tmElapsed*sin(m_curPos.h); 				G(2,2) = 0.0;
+  G(3,1) = tan(steerAngleRad)/m_wheelBase; 		G(3,2) = fDistRolled/(m_wheelBase * cos(steerAngleRad) * cos(steerAngleRad));
   m_logger->log(INFO, "Calculated F and G: ");
 	
-	//Now calculate the probabilit matrix for the position.
+	//Now calculate the probability matrix for the position.
 	m_P = F * m_P * F.transpose() + G * m_Q * G.transpose();
-	
-	//We really only want to use GPS information if enough time has passed.
-	int curSec = getGPSTimeSec();
   
-	double curLat = m_curPos.y;
-	double curLon = m_curPos.x;
-	double curHed = m_curPos.h;
-	if (tmElapsed > 1200 && curSec != m_curGPSTimeSec && getGPSQuality()) {
+  //We really only want to use GPS information if enough time has passed.
+	if ((curClock - m_gpsClock) > 500 && m_pGPS->getQuality()) {
 	
-		//Grab current GPS and compass settings, otherwise we'll rely on 
-		//a Kalman update with encoder values only.
-		curLat = getGPSLatitude();
-		curLon = getGPSLongitude();
-		curHed = getHeading();
+    float curLon, curLat, curHed;
+    //If we got any invalid BS, don't do this update.
+    if(m_pGPS->getPosition(&curLon, &curLat, &curHed) == aErrNone) {
+    	// We fall back to GPS heading here, we'll report an error.
+      if(0 != m_pCompass->getHeadingDeg(&curHed))
+        m_logger->log(INFO, "Error getting heading from compass");
 		
-		//Lets to this KF thing.
-		Matrix V(3, 1);
-		V(1,1) = curLat - state(1,1);
-		V(2,1) = curLon - state(2,1);
-		V(3,1) = curHed - state(3,1);
+      //Lets to this KF thing.
+			Matrix V(3, 1);
+			V(1,1) = curLon - state(1,1);
+			V(2,1) = curLat - state(2,1);
+			V(3,1) = curHed - state(3,1);
 	  
-	  Matrix S(3, 3), R(3, 3);
-	  S = m_P + m_W;
-	  R = m_P * S.invert();
-	  state = state + R * V;
+      Matrix S(3, 3), R(3, 3);
+      S = m_P + m_W;
+      R = m_P * S.invert();
+      state = state + R * V;
 	  
-	  m_P = m_P - R * m_P;
+	  	m_P = m_P - R * m_P;
 	
-		fprintf(gps_track, "%3.12f, %2.12f, %3.1f\n", 
-		       curLon, curLat, curHed);
-		m_curGPSTimeSec = curSec;
-		
-		m_curClock = clock();
+			m_gpsClock = curClock;
+    }
 	}
 
 	//Localization is done. Update the current robot state.
-	m_curPos.x = state(2, 1);
-	m_curPos.y = state(1, 1);
+	m_curPos.x = state(1, 1);
+	m_curPos.y = state(2, 1);
 	m_curPos.h = state(3, 1);
-	*/
+	
 	 
 }
 
